6d11a6460facc9f4aa8f1d0302979fa880b8a0dc
Chaoticwave-boop <bren.stoute@hotmail.com>
2023-03-17 13:33:39 +0100

    same thing but with get instead of post

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -1,4 +1,11 @@
 <?php
-   echo "<h2>your name = {$_POST["fname"]}</h2>";
-   echo "<h2>your email = {$_POST["femail"]}</h2>";
+//    echo "<h2>your name = {$_POST["fname"]}</h2>";
+//    echo "<h2>your email = {$_POST["femail"]}</h2>";
+
+
+//    echo "your name = " . $_GET['fname'] . " your emaill = " . $_GET['femail'];
+    echo "<h1>de ingevulde gegevens zijn : </h1>";
+
+    echo "<h2>your name = {$_GET["fname"]}</h2>";
+    echo "<h2>your email = {$_GET["femail"]}</h2>";
 ?>
